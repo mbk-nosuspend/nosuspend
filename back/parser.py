478ac59a0142f9c7c44a847529e46d809f61c0c6
Jose Contreras <joseangelcon55.jc@gmail.com>
2015-02-21 14:43:49 -0800

    Ethicity and discipline type

--- back/parser.py
+++ back/parser.py
@@ -16,6 +16,7 @@ data = []
 for line in data_file:
 	if not headers:
 		headers = True
+		continue
 
 	fields = line.split("\t")
 
@@ -25,7 +26,52 @@ for line in data_file:
 	incident['aggregate_level'] = aggregate_level
 
 	# TODO - parse other fields and add to map
+	cds=fields[1]
+	incident['cds'] = cds
+
+	name=fields[2]
+	incident['name'] = name
+
+	discipline_type=fields[3]
+	
+	if discipline_type=='E':
+		discipline_type='expulsion'
+	if discipline_type=='I':
+		discipline_type='In-School Suspension'
+	if discipline_type=='O':
+		discipline_type='Out-of-School Suspension'
+	incident['discipline_type'] = discipline_type
+
+	ethnicity= int(fields[4])
+	if ethnicity==0:
+		ethnicity='not recorded'
+	if ethnicity==1:
+		ethnicity='American Indian or Alaskan Native'
+	if ethnicity==2:
+		ethnicity='Asian'
+	if ethnicity==3:
+		ethnicity='Pacific Islander'
+	if ethnicity==4:
+		ethnicity='Filipino'
+	if ethnicity==5:
+		ethnicity='Hispanic/Latino'
+	if ethnicity==6:
+		ethnicity='African American'
+	if ethnicity==7:
+		ethnicity='White'
+	if ethnicity==9:
+		ethnicity='Two or more races'
+	incident['ethnicity'] =ethnicity
+
+
+
+
+
+
+
+
+
 
 	data.append(incident)
 
-print data
+print data[1]
