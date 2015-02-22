683f01db19a3e0310ad5f1e6731a22ff3c9c30ee
Patrick Marchwiak <pmarchwiak@gmail.com>
2015-02-21 18:27:24 -0800

    Add justified vs other data

--- back/db2json.py
+++ back/db2json.py
@@ -12,6 +12,15 @@ for row in c.execute("""SELECT ethnicity, SUM(total) FROM incidents WHERE name =
 
 	data['ethnicities'] = ethnicities
 
+c.execute("""SELECT SUM(num_weapons), SUM(drugs), SUM(violence_with_injury), SUM(violence_without_injury), SUM(other_non_defiance), 
+		SUM(other_defiance)
+		FROM incidents WHERE name = 'Oakland Unified';""")
+row = c.fetchone()
+
+
+data['justified_vs_other'] = {'weapons': row[0], 'drugs': row[1], 'violence_with_injury': row[2],
+	'violence_without_injury': row[3], 'other_non_defiance': row[4], 'other_defiance': row[5],
+	}
 
 print json.dumps(data)
 conn.close()
